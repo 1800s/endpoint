d8d9ea10060e196c3d4f3d4744fce7e58cb3c82f
velo <realfleqq@gmail.com>
2023-04-08 03:11:44 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
 const express = require('express');
 const { MongoClient } = require('mongodb');
+
 const app = express();
+const port = process.env.PORT || 3000;
 
 // Set up a MongoDB client and connect to the database
-const client = new MongoClient('mongodb+srv://est:yLvwzxRC1J3ozhJo@cluster0.rmvzzax.mongodb.net/?retryWrites=true&w=majority');
+const client = new MongoClient(process.env.MONGO_URI);
 client.connect();
+
+// Serve static files from the 'public' directory
+app.use(express.static('public', { index: 'index.html' }));
 
 // Define the /key/:key endpoint
 app.get('/key/:key', async (req, res) => {
   const key = req.params.key;
-  const db = client.db('mydb');
+  const db = client.db(process.env.MONGO_DB_NAME);
   const keys = db.collection('keys');
   const keyExists = await keys.findOne({ key: key });
 
@@ -20,6 +25,7 @@ app.get('/key/:key', async (req, res) => {
   }
 });
 
-app.listen(3000, () => {
-  console.log('Server listening on port 3000');
+// Start the server
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
 });
