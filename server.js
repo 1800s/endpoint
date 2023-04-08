cc1a51c0ff603a26887aba53123a74692fb5aa74
velo <realfleqq@gmail.com>
2023-04-08 02:29:03 -0700

    Update server.js

--- server.js
+++ server.js
@@ -3,14 +3,14 @@ const { MongoClient } = require('mongodb');
 const app = express();
 
 // Set up a MongoDB client and connect to the database
-const client = new MongoClient('<mongo_uri>');
+const client = new MongoClient('mongodb+srv://est:yLvwzxRC1J3ozhJo@cluster0.rmvzzax.mongodb.net/?retryWrites=true&w=majority');
 client.connect();
 
 // Define the /key/:key endpoint
 app.get('/key/:key', async (req, res) => {
   const key = req.params.key;
-  const db = client.db('<database_name>');
-  const keys = db.collection('<collection_name>');
+  const db = client.db('mydb');
+  const keys = db.collection('keys');
   const keyExists = await keys.findOne({ key: key });
 
   if (keyExists) {
